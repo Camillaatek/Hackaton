215cc008688d0fefc79282bf2dd10c3230fe78af
Krane96 <69842760+Krane96@users.noreply.github.com>
2022-03-10 12:57:46 +0100

    test

---
+++ src/pages/admin/Admin.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import StyledLink from '../components/nav/StyledLink';
+import { useEffect, useState } from 'react';
+import { BOOKINGS_PATH } from '../utils/api';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import useToggle from '../hooks/useToggle';
+import useAxios from '../hooks/useAxios';
+import AuthContext from '../context/AuthContext';
+import BookingsForm from '../components/admin/BookingsForm';
+import { Flex } from '../components/siteblocks/Flex';
+import BookingCard from '../components/siteblocks/BookingCard';
+
+const Admin = () => {
+  const [isTriggered, setIsTriggered] = useToggle();
+  const [error, setError] = useState();
+  const [bookings, setBookings] = useState([]);
+  const navigate = useNavigate();
+  const [auth, setAuth] = useContext(AuthContext);
+
+  const http = useAxios();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await http.get(BOOKINGS_PATH);
+      setBookings(data.data.data);
+    };
+
+    fetchData().catch((error) => setError(error.response.data.error));
+  }, [isTriggered, auth]);
+
+  const sendBooking = async (formData) => {
+    const options = {
+      data: {
+        title: formData.title,
+        message: formData.message,
+        contact: formData.contact,
+      },
+    };
+    const responseData = await http.post(BOOKINGS_PATH, options);
+    console.log(responseData);
+    setIsTriggered();
+  };
+
+  // if error object is populated, show user what happened and urge them to login
+  if (error) {
+    return (
+      <div>
+        <h1>You must be Authenticated to view this page</h1>
+        <h3>The server responded with: {error.status}</h3>
+        <p>{error.message}</p>
+        <p>Please Login</p>
+        <StyledLink to='/login'>Login</StyledLink>
+      </div>
+    );
+  }
+
+  const handleLogout = () => {
+    setAuth(null);
+    navigate('/login');
+  };
+
+  if (bookings.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <button onClick={handleLogout}>Logout</button>
+      <Flex flexDir='row' flexWrap='wrap' justifyContent='space-around'>
+        {bookings.map((item, idx) => {
+          const deleteBooking = async () => {
+            const responseData = await http.delete(
+              `${BOOKINGS_PATH}/${item.id}`
+            );
+            console.log(responseData);
+          };
+
+          const handleDelete = () => {
+            if (window.confirm('Are you sure?')) {
+              deleteBooking();
+              setIsTriggered();
+            } else {
+              return;
+            }
+          };
+          return (
+            <BookingCard key={idx}>
+              <p>{item.attributes.title} </p>
+              <StyledLink to={`/booking/${item.id}`}>EDIT</StyledLink>
+              <button onClick={handleDelete}>DELETE</button>
+            </BookingCard>
+          );
+        })}
+      </Flex>
+
+      <hr />
+      <BookingsForm sendBooking={sendBooking} />
+    </div>
+  );
+};
+
+export default Admin;
---
+++ src/utils/api.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://hackaton-hotel.herokuapp.com/';
+export const AUTH_URL = `${BASE_URL}api/auth/local`;
+export const BOOKINGS_URL = `${BASE_URL}api/hotels`;
+
+export const AUTH_PATH = 'api/auth/local';
+export const BOOKINGS_PATH = 'api/hotels';
